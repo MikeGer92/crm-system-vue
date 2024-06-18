9c9a520523d70a1a245d020df08401dc6d5f8f9b
MikeGer92 <MikeGer@yandex.ru>
2024-06-18 13:01:22 +0300

    added Loader, added AppWrite DB

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,7 +1,6 @@
 import { NuxtLink } from '../.nuxt/components';
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
-    <!-- TODO add shade CN -->
     <NuxtLink class="mb-10 block" to="/" >    
       <NuxtImg class="mx-auto" src="../public/images/logo.svg" alt="" width="100px"/>
     </NuxtLink >
