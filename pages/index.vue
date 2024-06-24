459161fed4c377e1cb5f48a07a6d196d4f4ea53e
MikeGer92 <MikeGer@yandex.ru>
2024-06-24 07:54:06 +0300

    added card to index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System</h1>
   </div>
-  <UiCard />
+  <UiCard class="mb-3" draggable="false">
+    <UiCardHeader role="button">name card</UiCardHeader>
+    <UiCardContent>Компания</UiCardContent>
+    <UiCardFooter>Date</UiCardFooter>
+  </UiCard>
 </template>
 
 <script setup lang="ts">
